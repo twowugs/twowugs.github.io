a085526b80f7dd352d1453090c0d0ade1432fd2d
twowugs <jakeclementinedavis@gmail.com>
2023-03-13 23:59:08 -0400

    test

--- experimental/experimental.js
+++ experimental/experimental.js
@@ -1,3 +1,10 @@
+var music = new Audio();
+function playSound() {
+    music.pause();
+    music = new Audio("Coqui.mp3");
+    music.play();
+}
+
 document.querySelectorAll('.grid-item').forEach(el, index); {
     if (index == num - 1) el.classList.add('onstate');
     else el.classList.remove('onstate');
