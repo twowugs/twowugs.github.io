ed3b5ab60c7f33642f7e1cd0a39c3611f1432d3e
twowugs <jakeclementinedavis@gmail.com>
2023-03-14 00:26:00 -0400

    op1 pads colored

--- experimental/experimental.js
+++ experimental/experimental.js
@@ -1,9 +1,58 @@
+// I DONT KNOW LOOPS //
+function colorPad01() {
+  var element = document.getElementById("pad01");
+  element.classList.toggle("padClicked");
+} 
+
+function colorPad02() {
+  var element = document.getElementById("pad02");
+  element.classList.toggle("padClicked");
+} 
+
+function colorPad03() {
+  var element = document.getElementById("pad03");
+  element.classList.toggle("padClicked");
+} 
+
+function colorPad04() {
+  var element = document.getElementById("pad04");
+  element.classList.toggle("padClicked");
+} 
+
+function colorPad05() {
+  var element = document.getElementById("pad05");
+  element.classList.toggle("padClicked");
+} 
+
+function colorPad06() {
+  var element = document.getElementById("pad06");
+  element.classList.toggle("padClicked");
+}
+
+function colorPad07() {
+  var element = document.getElementById("pad07");
+  element.classList.toggle("padClicked");
+}  
+
+function colorPad08() {
+  var element = document.getElementById("pad08");
+  element.classList.toggle("padClicked");
+}  
+
+function colorPad09() {
+  var element = document.getElementById("pad09");
+  element.classList.toggle("padClicked");
+}  
+
+// M U S I C S //
 var music = new Audio();
 function playSound() {
     music.pause();
     music = new Audio("Coqui.mp3");
     music.play();
 }
+
+
 
 document.querySelectorAll('.grid-item').forEach(el, index); {
     if (index == num - 1) el.classList.add('onstate');
