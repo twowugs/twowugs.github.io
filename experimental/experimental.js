ff8fe3c068f2e31918a0c079ee6c35956c359435
twowugs <jakeclementinedavis@gmail.com>
2023-03-13 23:41:21 -0400

    help!!

--- experimental/experimental.js
+++ experimental/experimental.js
@@ -2,8 +2,25 @@ document.querySelectorAll('.grid-item').forEach(el, index); {
     if (index == num - 1) el.classList.add('onstate');
     else el.classList.remove('onstate');
   }
-  
 
-  function onClick() {
-  classList.add('onClick');
-  }
+   //function onClick() {
+  //classList.add('onClick');
+  //}
+
+  //--------------------------------------------------
+
+  // E X P E R I M E N T I N G
+
+//  var colorPads = document.getElementsByClassName('grid-item');
+  //var modalPad = document.getElementById("pad01");
+
+  //for (var i = 0; i < colorPads.length; i++) {
+    //var col = colorPads[i];
+    // and attach our click listener for this image.
+    //col.onclick = function(evt) {
+      //scroll(0,0);
+      //pad.style.background-color = "#bfcde3ca";
+      //modalImg.src = this.src;
+    //}
+  
+ // }
