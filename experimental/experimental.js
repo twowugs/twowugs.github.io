83bc20f3437e645062066adb9ab7f39d0543d90b
twowugs <jakeclementinedavis@gmail.com>
2023-03-15 20:07:05 -0400

    OP1 DrumPad

--- experimental/experimental.js
+++ experimental/experimental.js
@@ -2,55 +2,91 @@
 function colorPad01() {
   var element = document.getElementById("pad01");
   element.classList.toggle("padClicked");
+  var ateOhAte = new Audio();
+  ateOhAte.pause();
+  ateOhAte = new Audio("808.wav");
+  ateOhAte.play();
 } 
 
 function colorPad02() { 
   var element = document.getElementById("pad02");
   element.classList.toggle("padClicked");
+  var Bass01 = new Audio();
+  Bass01.pause();
+  Bass01 = new Audio("Bass01.wav");
+  Bass01.play();
 } 
 
 function colorPad03() {
   var element = document.getElementById("pad03");
   element.classList.toggle("padClicked");
+  var Bass02 = new Audio();
+  Bass02.pause();
+  Bass02 = new Audio("Bass02.wav");
+  Bass02.play();
 } 
 
 function colorPad04() {
   var element = document.getElementById("pad04");
   element.classList.toggle("padClicked");
+  var Clap01 = new Audio();
+  Clap01.pause();
+  Clap01 = new Audio("Clap01.wav");
+  Clap01.play();
 } 
 
 function colorPad05() {
   var element = document.getElementById("pad05");
   element.classList.toggle("padClicked");
+  var Crash01 = new Audio();
+  Crash01.pause();
+  Crash01 = new Audio("Crash01.wav");
+  Crash01.play();
 } 
 
 function colorPad06() {
   var element = document.getElementById("pad06");
   element.classList.toggle("padClicked");
+  var Hat01 = new Audio();
+  Hat01.pause();
+  Hat01 = new Audio("Hat01.wav");
+  Hat01.play();
 }
 
 function colorPad07() {
   var element = document.getElementById("pad07");
   element.classList.toggle("padClicked");
+  var Kick01 = new Audio();
+  Kick01.pause();
+  Kick01 = new Audio("Kick01.wav");
+  Kick01.play();
 }  
 
 function colorPad08() {
   var element = document.getElementById("pad08");
   element.classList.toggle("padClicked");
+  var Perc01 = new Audio();
+  Perc01.pause();
+  Perc01 = new Audio("Perc01.wav");
+  Perc01.play();
 }  
 
 function colorPad09() {
   var element = document.getElementById("pad09");
   element.classList.toggle("padClicked");
+  var Snare01 = new Audio();
+  Snare01.pause();
+  Snare01 = new Audio("Snare01.wav");
+  Snare01.play();
 }  
 
 // M U S I C S //
-var music = new Audio();
-function playSound() {
-    music.pause();
-    music = new Audio("Coqui.mp3");
-    music.play();
-}
+//var music = new Audio();
+//function playSound() {
+  //  music.pause();
+    //music = new Audio("Coqui.mp3");
+    //music.play();
+//}
 
 
 
