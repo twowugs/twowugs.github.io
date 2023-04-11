8371a933ba39181a237f97e58249530b1b226bd3
twowugs <jakeclementinedavis@gmail.com>
2023-04-11 14:49:06 -0400

    updating mobile navbar

---
+++ portfolio/mobile.js
@@ -0,0 +1,28 @@
+//span.onclick = function() {
+  //  modal.style.display = "none";
+//  }
+
+//function toggleNavbar() {
+  //  var element = document.getElementById("mobile-dropdown-content");
+    //element.classList.toggle("mobile-js");
+  //}  
+
+  function toggleNavbar() {
+    var x = document.getElementById("mobile-dropdown-content");
+    if (x.style.display === "none") {
+      x.style.display = "grid";
+      var element = document.getElementById("arrow01");
+      element.classList.add("visible");
+      var element = document.getElementById("arrow02");
+      element.classList.add("visible");
+      var element = document.getElementById("arrow03");
+      element.classList.add("visible");
+      var element = document.getElementById("arrow04");
+      element.classList.add("visible");
+      var element = document.getElementById("arrow05");
+      element.classList.add("visible");
+    } else {
+      x.style.display = "none";
+    }
+    
+  }
