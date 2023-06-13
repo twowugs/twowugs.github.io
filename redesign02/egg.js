4b3aae05ab0e430db2980795429db015d2c64ba2
twowugs <jakeclementinedavis@gmail.com>
2023-06-12 20:58:03 -0400

    two weeks of stuff i shouldve pushed

---
+++ redesign02/egg.js
@@ -0,0 +1,18 @@
+
+//show advertisements
+function showAdSpace() {
+    var a = document.getElementById("adSpace-grid");
+      a.style.display = "grid";
+  } 
+
+//hide advertisement
+function hideAdSpace01() {
+    var b = document.getElementById("adSpace01");
+      b.style.display = "none";
+    
+  } 
+
+  function hideAdSpace02() {
+    var d = document.getElementById("adSpace02");
+      d.style.display = "none";
+  } 
