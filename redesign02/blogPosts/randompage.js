d8b09ef524e7b5dcfe0dacdbcd5d4afd21a98080
twowugs <jakeclementinedavis@gmail.com>
2023-09-01 03:48:58 -0400

    yeah sure i'll push this now

---
+++ redesign02/blogPosts/randompage.js
@@ -0,0 +1,29 @@
+
+var a= document.getElementById ("hover-text01");
+      a.style.color = "#212121";
+var a= document.getElementById ("hover-text02");
+      a.style.color = "#212121";
+var a= document.getElementById ("hover-text03");
+      a.style.color = "#212121";
+
+
+function toggleSheep01() {
+    var a= document.getElementById ("hover-text01");
+      a.style.color = "#547973";
+      var bouba= document.getElementById ("bouba");
+      bouba.style.color = "#a8a391";
+}
+
+function toggleSheep02() {
+    var a= document.getElementById ("hover-text02");
+      a.style.color = "#547973";
+      var hound= document.getElementById ("hound");
+      hound.style.color = "#a8a391";
+}
+
+function toggleSheep03() {
+    var a= document.getElementById ("hover-text03");
+      a.style.color = "#547973";
+      var trout= document.getElementById ("trout");
+      trout.style.color = "#a8a391";
+}
